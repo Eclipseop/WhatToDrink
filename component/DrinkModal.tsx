ac0725d7bee84e8d59e491d637feea55fe60b405
eclipseop <mh@otterdev.com>
2021-08-23 13:16:58 -0400

    ⭐ Use font-awesome star icon instead of hardcoded SVG.

--- component/DrinkModal.tsx
+++ component/DrinkModal.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Cocktail, Ingredient } from '../pages';
 import axios from 'axios';
 import { useSession } from 'next-auth/client';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface Props {
     cocktail: Cocktail;
@@ -65,15 +67,12 @@ const DrinkModal: React.FC<Props> = (props: Props) => {
                         <a className="font-semibold transition delay-75 ease-out hover:text-yellow-500" href={`/drink/${cocktail.id}`}>{cocktail.name}</a>
                         <p className="text-gray-500 text-xs">{desc()}</p>
                     </div>
-                    <svg
+                    <FontAwesomeIcon 
                         onClick={() => session ? (isFavorite() ? submitRemoveFavorite() : submitAddFavorite()) : alert('Please login to add favorites.')}
-                        xmlns="http://www.w3.org/2000/svg"
                         className={`h-5 w-5 flex-none fill-current
                         ${isFavorite() ? 'text-yellow-300 hover:text-gray-300' : 'text-gray-300 hover:text-yellow-300'}`}
-                        viewBox="0 0 20 20"
-                    >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
+                        icon={faStar}
+                    />
                 </div>
                 <div className="flex">
                     <ul className="list-disc list-inside px-1">
