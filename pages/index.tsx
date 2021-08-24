399ce7e40c40997416cbfb38c5db359180384c94
eclipseop <mh@otterdev.com>
2021-08-24 11:47:51 -0400

    🖼 Add favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -156,6 +156,12 @@ const Index: React.FC<SearchProps> = (props: SearchProps) => {
                 <title>Home | WhatToDrink</title>
                 <meta property="og:title" content="Home | WhatToDrink" key="title" />
                 <meta name="description" content="WhatToDrink is the best way to find new drinks! Input ingredients and see what you can make!" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <Header />
             <div className="flex flex-row flex-1">
