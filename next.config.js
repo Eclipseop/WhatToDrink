193c1a697467399f69cbbc0dae815b4e9245950e
eclipseop <mh@otterdev.com>
2021-08-24 15:16:28 -0400

    🎭 Switch GitHub login to Google

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        domains: ['www.thecocktaildb.com', 'avatars.githubusercontent.com'],
+        domains: ['www.thecocktaildb.com', 'avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
     }
 };
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,9 +5,13 @@ import prisma from '../../../prisma/db';
 
 export default NextAuth({
     providers: [
-        Providers.GitHub({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        // Providers.GitHub({
+        //     clientId: process.env.GITHUB_CLIENT_ID,
+        //     clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        // }),
+        Providers.Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
 
